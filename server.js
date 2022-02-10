e03ac9f860ccf93aa8cd545cc32fe156f71439b7
Adam <adam.shelley18@gmail.com>
2022-02-10 21:10:34 +0800

    updated port

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ mongoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vtmwj.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(PORT, () => {
+    app.listen(process.env.PORT || PORT, () => {
       console.log(`Listening on port ${PORT}`);
     });
   })
